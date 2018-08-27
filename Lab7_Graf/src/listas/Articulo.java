59ed8728bead4c14e24cf8cfa0d048fd6c91eea6
Oliver Graf <gra17190@uvg.edu.gt>
2018-08-27 11:36:22 -0600

    Primer commit lab 7

---
+++ Lab7_Graf/src/listas/Articulo.java
@@ -0,0 +1,19 @@
+package listas;
+
+public class Articulo {
+    private int cantidad;
+    private int precio;
+    private String nombre;
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
---
+++ Lab7_Graf/src/listas/Lista.java
@@ -0,0 +1,17 @@
+package listas;
+
+import java.util.ArrayList;
+
+public class Lista {
+    private String descripcion;
+    private String fecha;
+    private ArrayList<Articulo>;
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+}
